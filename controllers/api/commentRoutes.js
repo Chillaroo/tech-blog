eb58ea174780f790c5b574eb203756e3e1884bfc
Rachel Chin <rtc145@gmail.com>
2021-09-06 21:58:34 -0400

    built user routes - still need to test

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,2 @@
+//GET all comments by post_id
+//POST create one comment by post_id
---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,5 @@
+//GET all posts
+//GET all posts by user_id
+//GET one post by id and include comment data
+//PUT update one post by id
+//DELETE one post by id
